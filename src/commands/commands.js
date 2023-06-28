7e88d6fa64442a566cde8824772547c90caaf510
Steshenko Dmitry <3301297@gmail.com>
2023-06-28 19:36:20 +0300

    feat: implement /dog and /cat commands using image api

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,3 +1,5 @@
+import { getRandomImage } from '../api/imagesApi.js';
+
 const start = (ctx) => {
   const username = ctx.message.from.first_name || ctx.message.from.username;
   ctx.reply(`Welcome, ${username}`);
@@ -5,7 +7,19 @@ const start = (ctx) => {
 
 const help = (ctx) => ctx.reply('Weather');
 const weather = (ctx) => ctx.reply('Weather');
-const dog = (ctx) => ctx.reply('Dog image');
-const cat = (ctx) => ctx.reply('Cat image');
+const dog = async (ctx) => {
+  const imageInfo = await getRandomImage('dog');
+  ctx.replyWithPhoto(
+    { url: imageInfo.urls.raw },
+    { caption: imageInfo.alt_description },
+  );
+};
+const cat = async (ctx) => {
+  const imageInfo = await getRandomImage('cat');
+  ctx.replyWithPhoto(
+    { url: imageInfo.urls.raw },
+    { caption: imageInfo.alt_description },
+  );
+};
 
 export { start, help, weather, dog, cat };
