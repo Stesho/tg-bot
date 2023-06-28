f8d40c4e0da30d87399185a86664c82568f1a2e1
Steshenko Dmitry <3301297@gmail.com>
2023-06-28 19:30:15 +0300

    feat: add image api constants

--- src/api/imagesApi.js
+++ src/api/imagesApi.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
 import process from 'node:process';
+import {
+  BASE_IMAGE_API_URL as BASE_URL,
+  RANDOM_IMAGE_API_URL as RANDOM_IMAGE_URL,
+} from '../constants/imageApiConstants.js';
 
-const URL = 'https://api.unsplash.com/photos/random';
 const getRandomImage = async (query) => {
-  const response = await axios.get(`${URL}?query=${query}&count=1`, {
-    headers: {
-      Authorization: `Client-ID ${process.env.UNSPLASH_API_TOKEN}`,
+  const imageCount = 1;
+  const response = await axios.get(
+    `${BASE_URL}${RANDOM_IMAGE_URL}?query=${query}&count=${imageCount}`,
+    {
+      headers: {
+        Authorization: `Client-ID ${process.env.UNSPLASH_API_TOKEN}`,
+      },
     },
-  });
+  );
   return response.data[0];
 };
 
---
+++ src/constants/imageApiConstants.js
@@ -0,0 +1,2 @@
+export const BASE_IMAGE_API_URL = 'https://api.unsplash.com';
+export const RANDOM_IMAGE_API_URL = '/photos/random';
