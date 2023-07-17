24d82e99691260e9558b367a159a31ce32a24d45
Steshenko Dmitry <3301297@gmail.com>
2023-07-17 19:22:56 +0300

    refactor: move get random image by query to command file

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,8 +1,24 @@
-import { getRandomImageByQuery } from '../services/imagesService.js';
 import { WEATHER_SCENE } from '../constants/scenes/weatherScenesConst.js';
 import { TASKS_SCENE } from '../constants/scenes/tasksScenesConst.js';
 import { RECOMMEND_SCENE } from '../constants/scenes/recommendScenesConst.js';
 import textMessages from '../constants/messages/textMessages.js';
+import repliesMessages from '../constants/messages/repliesMessages.js';
+import getRandomImage from '../api/images/getRandomImage.js';
+
+const getRandomImageByQuery = async (ctx, query) => {
+  const loadingMessage = await ctx.reply(repliesMessages.loading);
+  const resources = await getRandomImage(query);
+
+  if (resources.isError) {
+    return ctx.reply(resources.data);
+  }
+
+  const imageInfo = resources.data.photos[0];
+  await ctx.replyWithPhoto({
+    url: imageInfo.src.medium || imageInfo.src.large,
+  });
+  ctx.deleteMessage(loadingMessage.message_id, loadingMessage.chat.id);
+};
 
 const start = (ctx) => {
   const username = ctx.message.from.first_name || ctx.message.from.username;
