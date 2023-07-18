8a56b73efc6375b832259d62803a17f854d060cf
Steshenko Dmitry <3301297@gmail.com>
2023-07-18 22:15:32 +0300

    fix: correct server time

--- src/utils/getCurrentTime.js
+++ src/utils/getCurrentTime.js
@@ -1,6 +1,9 @@
 const getCurrentTime = () => {
   const currentDate = new Date();
-  const hours = currentDate.getHours().toString().padStart(2, '0');
+  const serverTimeShift = 3;
+  const hours = (currentDate.getHours() + serverTimeShift)
+    .toString()
+    .padStart(2, '0');
   const minutes = currentDate.getMinutes().toString().padStart(2, '0');
   return `${hours}:${minutes}`;
 };
